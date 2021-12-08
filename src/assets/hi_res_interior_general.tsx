0ebf2fd6352801b46630d1be1931ad26d3af92ee
Thomas Mielke <mielket@gmx.de>
2021-12-08 05:52:16 +0100

    concert hall

---
+++ src/assets/hi_res_interior_general.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="hi_res_interior_general" tilewidth="32" tileheight="32" tilecount="4792" columns="8">
+ <image source="hi_res_interior_general.png" width="256" height="19168"/>
+</tileset>
