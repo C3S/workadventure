26c40bae67c9d31715f74e55b9eb8b0214760b63
thomiel <mielket@gmx.de>
2021-12-08 18:50:58 +0000

    Deploying to gh-pages from master @ ed6e70005d01e3714056eabe511258689494aba6 🚀

--- src/assets/hi_res_interior_general.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="hi_res_interior_general" tilewidth="32" tileheight="32" tilecount="4792" columns="8">
- <properties>
-  <property name="titlesetCopyright" value="by akizakura16 on deviantart.com: &quot;free for non-commercial use&quot;"/>
- </properties>
- <image source="hi_res_interior_general.png" width="256" height="19168"/>
-</tileset>
