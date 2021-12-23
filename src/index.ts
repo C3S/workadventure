f135c4d7b343fc24d65760638982334389c399ea
thomiel <mielket@gmx.de>
2021-12-23 01:20:43 +0000

    Deploying to gh-pages from master @ e015ab8cefb243833f9b17d7678ccbfa84f4ce46 🚀

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,12 @@ const time = today.getHours() + ":" + today.getMinutes();
 WA.room.onEnterZone('clock', () => {
     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
-
 WA.room.onLeaveZone('clock', closePopUp)
+
+WA.room.onEnterZone('djbooth', () => {
+    currentPopup =  WA.ui.openPopup("djboothPopup","Now playing: Nine Inch Nails (from the album The Slip, CC BY-NC-SA)",[]);
+})
+WA.room.onLeaveZone('djbooth', closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
