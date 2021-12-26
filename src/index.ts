415a4311b5e3d9629604dbd463a8a246a52e765f
thomiel <mielket@gmx.de>
2021-12-26 19:03:08 +0000

    Deploying to gh-pages from master @ 2b7c2aa0db82511d29fae6f505ee48f582d3a6f5 🚀

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,10 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
+// popups
 let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
-
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-WA.room.onLeaveZone('clock', closePopUp)
-
-WA.room.onEnterZone('djbooth', () => {
-    currentPopup =  WA.ui.openPopup("djboothPopup","Now playing: Nine Inch Nails (from the album The Slip, CC BY-NC-SA)",[]);
-})
-WA.room.onLeaveZone('djbooth', closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
@@ -25,3 +16,55 @@ function closePopUp(){
         currentPopup = undefined;
     }
 }
+
+WA.room.onEnterZone('clock', () => {
+    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+});
+
+WA.room.onLeaveZone('clock', closePopUp);
+
+WA.room.onEnterZone('djbooth', () => {
+    currentPopup =  WA.ui.openPopup("djboothPopup","Now playing: Nine Inch Nails (from the album The Slip, CC BY-NC-SA)",[]);
+});
+WA.room.onLeaveZone('djbooth', closePopUp);
+
+// chatbots
+
+let currentBot: any = undefined;
+let lastMessage: string = "";
+
+let recruitmentWelcome: boolean = false;
+const recruitmentWelcomeMessage = "Greetings, mortal! What a nice day to join C3S, isn't it? You may proceed to the key, if you want to apply for membership. You can ask me one-word questions beginning with 'w' otherwise.";
+const rectuitmentDontKnow = "I don't know an answer to '";
+const recruitmentPrompt = "If I understand you right, you are asking: ";
+WA.room.onEnterZone('recruitment', () => {
+    currentBot = "Recruitment Officer";
+    if (!recruitmentWelcome)
+        WA.chat.sendChatMessage(recruitmentWelcomeMessage, currentBot);
+    else
+        recruitmentWelcome = true;
+});
+WA.room.onLeaveZone('recruitment',  () => {
+    currentBot = undefined;
+});
+function recruitmentAnswer(m: string) {
+    if (m == "why" || m == "warum") return recruitmentPrompt + "Why you should become a member? If you are a music-prducing being, C3S needs your repertoire *now* to proof to autorities that it can sustain its business as a collecting society. Best soultion for this is to become a full member. Of course, if you don't do music, you can become investing member to support us in other ways (you will not have the right to vote on general assemblies but you may attend there).";
+    if (m == "when" || m == "wann") return recruitmentPrompt + "When will be the best time to join? Now, of course! Oh, you mean when will we start operation?... Well, we spent 2021 writing a very sophisticated business plan to be approved by the authorities and maybe 2022 we'll start with a live tariff. (I know we sometimes used to tell people 'next year' or so before, when we were not aware of all implications, but now it may actually be within reach.)";
+    if (m == "where" || m == "wo") return recruitmentPrompt + "Where we will become operational? First, in Germany; then we'll start field offices in other European countries. Currently our office is located in Düsseldorf.";
+    if (m == "what" || m == "was") return recruitmentPrompt + "What is C3S all about? Now... C3S is about to become a music collective society. A collecting societies task is to allow everyone to use copyright-protected works without the user making contracts with each and every copyright holder. The user needs to license the use of works unter certain tariffs. C3S' speciality is that it allows the use of CC licenses -- but we want to also make a difference in other aspects like transparency or freedom to which works a rightsholder wants to let us manage.";
+    //if (m == "w") return recruitmentPrompt + "";
+    //if (m == "w") return recruitmentPrompt + "";
+
+    return rectuitmentDontKnow + m + "'";
+}
+
+WA.chat.onChatMessage((m => {
+    if (currentBot == "Recruitment Officer")
+        if (m != recruitmentWelcomeMessage && m != lastMessage
+            && m.substring(0, rectuitmentDontKnow.length) != rectuitmentDontKnow
+            && m.substring(0, recruitmentPrompt.length) != recruitmentPrompt)
+        {
+            lastMessage = m;
+            WA.chat.sendChatMessage(recruitmentAnswer(m), currentBot);
+        }
+}));
